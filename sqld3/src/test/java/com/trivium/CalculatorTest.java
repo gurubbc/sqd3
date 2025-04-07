89bd0e5996f04b616fb78c51643f2dede7daf6cd
Guru <gurumurthy.ramamurthy@gmail.com>
2025-04-07 15:18:26 +0530

    commit 1

---
+++ sqld3/src/test/java/com/trivium/CalculatorTest.java
@@ -0,0 +1,39 @@
+package com.trivium;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
+/*
+ * This is the unit test code for the calculator.
+ */
+class CalculatorTest {
+
+//	@Test 
+//	void testAdd() {
+//		Calculator calculator = new Calculator();
+//		int result = calculator.add(2, 3);
+//		assertEquals(5, result); // will fail
+//	}
+
+	@Test
+	void testSubtract() {
+		Calculator calculator = new Calculator();
+		int result = calculator.subtract(5, 3);
+		assertEquals(2, result);
+	}
+	
+	@Test
+	void testMultiply() {
+		Calculator calculator = new Calculator();
+		int result = calculator.multiply(20, 2);
+		assertEquals(40, result);
+	}
+	
+	@Test
+	void testDivide() {
+		Calculator calculator = new Calculator();
+		int result = calculator.divide(25, 5);
+		assertEquals(5, result);
+	}
+}

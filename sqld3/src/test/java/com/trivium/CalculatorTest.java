08a5bd05ee128fb5129ea0e032f1e3718468d7e4
Guru <gurumurthy.ramamurthy@gmail.com>
2025-04-07 16:03:32 +0530

    100% code coverage with quality gate

--- sqld3/src/test/java/com/trivium/CalculatorTest.java
+++ sqld3/src/test/java/com/trivium/CalculatorTest.java
@@ -9,26 +9,26 @@ import org.junit.jupiter.api.Test;
  */
 class CalculatorTest {
 
-//	@Test 
-//	void testAdd() {
-//		Calculator calculator = new Calculator();
-//		int result = calculator.add(2, 3);
-//		assertEquals(5, result); // will fail
-//	}
-//
-//	@Test 
-//	void testSubtract() {
-//		Calculator calculator = new Calculator();
-//		int result = calculator.subtract(5, 3);
-//		assertEquals(2, result);
-//	} 
-//	
-//	@Test
-//	void testMultiply() {
-//		Calculator calculator = new Calculator();
-//		int result = calculator.multiply(20, 2);
-//		assertEquals(40, result);
-//	} 
+	@Test 
+	void testAdd() {
+		Calculator calculator = new Calculator();
+		int result = calculator.add(2, 3);
+		assertEquals(5, result); // will fail
+	}
+
+	@Test 
+	void testSubtract() {
+		Calculator calculator = new Calculator();
+		int result = calculator.subtract(5, 3);
+		assertEquals(2, result);
+	} 
+	
+	@Test
+	void testMultiply() {
+		Calculator calculator = new Calculator();
+		int result = calculator.multiply(20, 2);
+		assertEquals(40, result);
+	} 
 	
 	@Test
 	void testDivide() {
